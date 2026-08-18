aa7814166351293132abd2a943537708c9b93486
bwesseljr <bryanwesseljr@gmail.com>
2026-08-18 17:33:42 -0400

    mount college career agenda snapshot

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import WeekSetupPortal from './components/WeekSetupPortal.jsx'
+import CollegeCareerAgendaCardPortal from './components/CollegeCareerAgendaCardPortal.jsx'
 import './index.css' // <-- Make sure this line is here!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <WeekSetupPortal />
+    <CollegeCareerAgendaCardPortal />
   </React.StrictMode>,
 )

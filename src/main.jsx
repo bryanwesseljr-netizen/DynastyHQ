c35f4555db48b946c226ccdbeb9d94001db51a02
bwesseljr <bryanwesseljr@gmail.com>
2026-08-18 17:09:46 -0400

    mount weekly agenda week setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import WeekSetupPortal from './components/WeekSetupPortal.jsx'
 import './index.css' // <-- Make sure this line is here!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <WeekSetupPortal />
   </React.StrictMode>,
 )

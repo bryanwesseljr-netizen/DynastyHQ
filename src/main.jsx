786823e8eb1c2e0b19030810c24b57cbf3b5350c
bwesseljr <bryanwesseljr@gmail.com>
2026-08-23 18:45:10 -0400

    load chronicle polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './newsroom-bearcats-logo.css'
 import './weekly-agenda-v3-refinements.css'
 import './newsroom-polish-v4.css'
 import './podcast-polish-v4.css'
+import './chronicle-polish-v4.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

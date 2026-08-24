359c6d4b461221a27fa7da8e12bffdeddc397f53
bwesseljr <bryanwesseljr@gmail.com>
2026-08-23 20:26:31 -0400

    centralize owner-only enhancements

---
+++ src/components/OwnerEnhancements.jsx
@@ -0,0 +1,23 @@
+import WeekSetupPortal from './WeekSetupPortal.jsx';
+import GameweekFlowPortal from './GameweekFlowPortal.jsx';
+import QuickImportPortal from './QuickImportPortal.jsx';
+import WeeklyAgendaV2Portal from './WeeklyAgendaV2Portal.jsx';
+import CollegeCareerAgendaCardPortal from './CollegeCareerAgendaCardPortal.jsx';
+import RtgStatusScannerPortal from './RtgStatusScannerPortal.jsx';
+import CoachRecruitingWorkspaceV2Portal from './CoachRecruitingWorkspaceV2Portal.jsx';
+import PodcastHumanizedAudioPortal from './PodcastHumanizedAudioPortal.jsx';
+
+const OwnerEnhancements = () => (
+  <>
+    <WeekSetupPortal />
+    <GameweekFlowPortal />
+    <QuickImportPortal />
+    <WeeklyAgendaV2Portal />
+    <CollegeCareerAgendaCardPortal />
+    <RtgStatusScannerPortal />
+    <CoachRecruitingWorkspaceV2Portal />
+    <PodcastHumanizedAudioPortal />
+  </>
+);
+
+export default OwnerEnhancements;

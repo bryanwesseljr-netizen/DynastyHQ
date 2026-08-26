81a96b68639d66c52ee21b00f1b8c1146f3534cd
bwesseljr <bryanwesseljr@gmail.com>
2026-08-26 00:27:18 -0400

    Add complete 2026 FBS team media identities

---
+++ src/domain/fbsTeamMediaProfiles.js
@@ -0,0 +1,173 @@
+// 2026 FBS program identity catalog used by DynastyHQ's dynamic team media layer.
+// Format: school|nickname|city|primary|secondary
+const PROFILE_ROWS = `
+Boston College|Eagles|Chestnut Hill|#98002E|#BC9B6A
+California|Golden Bears|Berkeley|#003262|#FDB515
+Clemson|Tigers|Clemson|#F56600|#522D80
+Duke|Blue Devils|Durham|#00539B|#FFFFFF
+Florida State|Seminoles|Tallahassee|#782F40|#CEB888
+Georgia Tech|Yellow Jackets|Atlanta|#B3A369|#003057
+Louisville|Cardinals|Louisville|#AD0000|#000000
+Miami|Hurricanes|Coral Gables|#005030|#F47321
+NC State|Wolfpack|Raleigh|#CC0000|#000000
+North Carolina|Tar Heels|Chapel Hill|#7BAFD4|#FFFFFF
+Pittsburgh|Panthers|Pittsburgh|#003594|#FFB81C
+SMU|Mustangs|University Park|#C8102E|#354CA1
+Stanford|Cardinal|Stanford|#8C1515|#FFFFFF
+Syracuse|Orange|Syracuse|#D44500|#000E54
+Virginia|Cavaliers|Charlottesville|#232D4B|#F84C1E
+Virginia Tech|Hokies|Blacksburg|#630031|#CF4420
+Wake Forest|Demon Deacons|Winston-Salem|#9E7E38|#000000
+Arizona|Wildcats|Tucson|#CC0033|#003366
+Arizona State|Sun Devils|Tempe|#8C1D40|#FFC627
+Baylor|Bears|Waco|#154734|#FFB81C
+BYU|Cougars|Provo|#002E5D|#FFFFFF
+Cincinnati|Bearcats|Cincinnati|#E00122|#050505
+Colorado|Buffaloes|Boulder|#CFB87C|#000000
+Houston|Cougars|Houston|#C8102E|#FFFFFF
+Iowa State|Cyclones|Ames|#C8102E|#F1BE48
+Kansas|Jayhawks|Lawrence|#0051BA|#E8000D
+Kansas State|Wildcats|Manhattan|#512888|#FFFFFF
+Oklahoma State|Cowboys|Stillwater|#FF7300|#000000
+TCU|Horned Frogs|Fort Worth|#4D1979|#A3A9AC
+Texas Tech|Red Raiders|Lubbock|#CC0000|#000000
+UCF|Knights|Orlando|#BA9B37|#000000
+Utah|Utes|Salt Lake City|#CC0000|#000000
+West Virginia|Mountaineers|Morgantown|#002855|#EAAA00
+Illinois|Fighting Illini|Champaign|#E84A27|#13294B
+Indiana|Hoosiers|Bloomington|#990000|#EEEDEB
+Iowa|Hawkeyes|Iowa City|#FFCD00|#000000
+Maryland|Terrapins|College Park|#E03A3E|#000000
+Michigan|Wolverines|Ann Arbor|#00274C|#FFCB05
+Michigan State|Spartans|East Lansing|#18453B|#FFFFFF
+Minnesota|Golden Gophers|Minneapolis|#7A0019|#FFCC33
+Nebraska|Cornhuskers|Lincoln|#D00000|#FFFFFF
+Northwestern|Wildcats|Evanston|#4E2A84|#FFFFFF
+Ohio State|Buckeyes|Columbus|#BB0000|#666666
+Oregon|Ducks|Eugene|#154733|#FEE123
+Penn State|Nittany Lions|State College|#041E42|#FFFFFF
+Purdue|Boilermakers|West Lafayette|#CEB888|#000000
+Rutgers|Scarlet Knights|Piscataway|#CC0033|#FFFFFF
+UCLA|Bruins|Los Angeles|#2774AE|#FFD100
+USC|Trojans|Los Angeles|#9D2235|#FFC72C
+Washington|Huskies|Seattle|#4B2E83|#B7A57A
+Wisconsin|Badgers|Madison|#C5050C|#FFFFFF
+Alabama|Crimson Tide|Tuscaloosa|#9E1B32|#FFFFFF
+Arkansas|Razorbacks|Fayetteville|#9D2235|#FFFFFF
+Auburn|Tigers|Auburn|#0C2340|#F26522
+Florida|Gators|Gainesville|#0021A5|#FA4616
+Georgia|Bulldogs|Athens|#BA0C2F|#000000
+Kentucky|Wildcats|Lexington|#0033A0|#FFFFFF
+LSU|Tigers|Baton Rouge|#461D7C|#FDD023
+Mississippi State|Bulldogs|Starkville|#660000|#FFFFFF
+Missouri|Tigers|Columbia|#F1B82D|#000000
+Oklahoma|Sooners|Norman|#841617|#FDF9D8
+Ole Miss|Rebels|Oxford|#CE1126|#14213D
+South Carolina|Gamecocks|Columbia|#73000A|#000000
+Tennessee|Volunteers|Knoxville|#FF8200|#FFFFFF
+Texas|Longhorns|Austin|#BF5700|#FFFFFF
+Texas A&M|Aggies|College Station|#500000|#FFFFFF
+Vanderbilt|Commodores|Nashville|#866D4B|#000000
+Army|Black Knights|West Point|#BFAF80|#000000
+Charlotte|49ers|Charlotte|#154734|#B9975B
+East Carolina|Pirates|Greenville|#592A8A|#FDC82F
+Florida Atlantic|Owls|Boca Raton|#002855|#D71920
+Memphis|Tigers|Memphis|#003087|#8D9093
+Navy|Midshipmen|Annapolis|#00205B|#C5B783
+North Texas|Mean Green|Denton|#00853E|#FFFFFF
+Rice|Owls|Houston|#00205B|#FFFFFF
+South Florida|Bulls|Tampa|#006747|#CFC493
+Temple|Owls|Philadelphia|#9E1B32|#FFFFFF
+Tulane|Green Wave|New Orleans|#006747|#00A9A5
+Tulsa|Golden Hurricane|Tulsa|#007AC1|#C5B783
+UAB|Blazers|Birmingham|#006341|#FFB81C
+UTSA|Roadrunners|San Antonio|#0C2340|#F15A22
+Delaware|Blue Hens|Newark|#033594|#E8CE31
+FIU|Panthers|Miami|#081E3F|#B6862C
+Jacksonville State|Gamecocks|Jacksonville|#A30D26|#FFFFFF
+Kennesaw State|Owls|Kennesaw|#FFC629|#000000
+Liberty|Flames|Lynchburg|#A6192E|#002D72
+Louisiana Tech|Bulldogs|Ruston|#0046AD|#E31B23
+Middle Tennessee|Blue Raiders|Murfreesboro|#0079C2|#C3C5C8
+Missouri State|Bears|Springfield|#5F0000|#E8E8E8
+New Mexico State|Aggies|Las Cruces|#862633|#FFFFFF
+Sam Houston|Bearkats|Huntsville|#E35205|#FFFFFF
+Western Kentucky|Hilltoppers|Bowling Green|#D50032|#FFFFFF
+Akron|Zips|Akron|#041E42|#A89968
+Ball State|Cardinals|Muncie|#BA0C2F|#FFFFFF
+Bowling Green|Falcons|Bowling Green|#FE5000|#4F2C1D
+Buffalo|Bulls|Buffalo|#005BBB|#FFFFFF
+Central Michigan|Chippewas|Mount Pleasant|#6A0032|#FFC82E
+Eastern Michigan|Eagles|Ypsilanti|#006633|#FFFFFF
+Kent State|Golden Flashes|Kent|#FDC82F|#003976
+Miami (OH)|RedHawks|Oxford|#BA0C2F|#FFFFFF
+Ohio|Bobcats|Athens|#00694E|#FFFFFF
+Sacramento State|Hornets|Sacramento|#00573C|#CDB97D
+Toledo|Rockets|Toledo|#15397F|#FFCC00
+UMass|Minutemen|Amherst|#881C1C|#FFFFFF
+Western Michigan|Broncos|Kalamazoo|#5A3825|#B5A167
+Air Force|Falcons|Colorado Springs|#0033A0|#FFFFFF
+Hawaii|Rainbow Warriors|Honolulu|#024731|#FFFFFF
+Nevada|Wolf Pack|Reno|#003366|#807F84
+New Mexico|Lobos|Albuquerque|#BA0C2F|#63666A
+NIU|Huskies|DeKalb|#C8102E|#000000
+North Dakota State|Bison|Fargo|#01402A|#FFC72C
+San Jose State|Spartans|San Jose|#0055A2|#E5A823
+UNLV|Rebels|Las Vegas|#BA0C2F|#666666
+UTEP|Miners|El Paso|#041E42|#FF8200
+Wyoming|Cowboys|Laramie|#492F24|#FFC425
+Boise State|Broncos|Boise|#0033A0|#D64309
+Colorado State|Rams|Fort Collins|#1E4D2B|#C8C372
+Fresno State|Bulldogs|Fresno|#D50032|#0033A0
+Oregon State|Beavers|Corvallis|#DC4405|#000000
+San Diego State|Aztecs|San Diego|#A6192E|#000000
+Texas State|Bobcats|San Marcos|#501214|#8D734A
+Utah State|Aggies|Logan|#041E42|#FFFFFF
+Washington State|Cougars|Pullman|#981E32|#5E6A71
+Appalachian State|Mountaineers|Boone|#000000|#FFCC00
+Arkansas State|Red Wolves|Jonesboro|#CC092F|#000000
+Coastal Carolina|Chanticleers|Conway|#007C92|#A27752
+Georgia Southern|Eagles|Statesboro|#041E42|#A99260
+Georgia State|Panthers|Atlanta|#0033A0|#CC0000
+James Madison|Dukes|Harrisonburg|#450084|#CBB677
+Louisiana|Ragin' Cajuns|Lafayette|#D50032|#000000
+UL Monroe|Warhawks|Monroe|#862633|#C99700
+Marshall|Thundering Herd|Huntington|#00A862|#FFFFFF
+Old Dominion|Monarchs|Norfolk|#041E42|#7C878E
+South Alabama|Jaguars|Mobile|#041E42|#BF0D3E
+Southern Miss|Golden Eagles|Hattiesburg|#FFC72C|#000000
+Troy|Trojans|Troy|#841617|#8A8D8F
+Notre Dame|Fighting Irish|Notre Dame|#0C2340|#C99700
+UConn|Huskies|Storrs|#041E42|#FFFFFF
+`.trim().split('\n');
+
+export const FBS_TEAM_MEDIA_PROFILES_2026 = Object.freeze(Object.fromEntries(
+  PROFILE_ROWS.map((row) => {
+    const [school, nickname, city, primary, secondary] = row.split('|');
+    return [school, Object.freeze({ nickname, city, primary, secondary })];
+  }),
+));
+
+export const FBS_TEAM_MEDIA_ALIASES_2026 = Object.freeze({
+  'App State': 'Appalachian State',
+  'Brigham Young': 'BYU',
+  'Central Florida': 'UCF',
+  'Connecticut': 'UConn',
+  'Florida International': 'FIU',
+  "Hawai'i": 'Hawaii',
+  'Hawaiʻi': 'Hawaii',
+  'Louisiana-Lafayette': 'Louisiana',
+  'Louisiana Lafayette': 'Louisiana',
+  'Louisiana-Monroe': 'UL Monroe',
+  'Louisiana Monroe': 'UL Monroe',
+  'Massachusetts': 'UMass',
+  'Miami (FL)': 'Miami',
+  'Miami Florida': 'Miami',
+  'North Carolina State': 'NC State',
+  'Northern Illinois': 'NIU',
+  'San José State': 'San Jose State',
+  'Southern Mississippi': 'Southern Miss',
+  'UTEP Miners': 'UTEP',
+  'UTSA Roadrunners': 'UTSA',
+  'West Point': 'Army',
+});

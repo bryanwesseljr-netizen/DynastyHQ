025a5a8e3138090c0f2f6fe76eb152084a0cf770
bwesseljr <bryanwesseljr@gmail.com>
2026-09-14 23:12:02 -0400

    Attach evidence-driven Story Director to Game Hub

--- src/components/GameHubPortal.jsx
+++ src/components/GameHubPortal.jsx
@@ -1,10 +1,12 @@
 import GameHubPortalBase from './GameHubPortalBase.jsx';
 import GameHubOfficialMediaBridgePortal from './GameHubOfficialMediaBridgePortal.jsx';
+import GameHubStoryDirectorBridgePortal from './GameHubStoryDirectorBridgePortal.jsx';
 
 const GameHubPortal = () => (
   <>
     <GameHubPortalBase />
     <GameHubOfficialMediaBridgePortal />
+    <GameHubStoryDirectorBridgePortal />
   </>
 );
 

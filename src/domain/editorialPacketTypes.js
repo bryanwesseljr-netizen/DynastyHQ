898b948f97517d1c7b9e3758480ba87fa4daf482
bwesseljr <bryanwesseljr@gmail.com>
2026-09-14 23:00:07 -0400

    Remove temporary editorial packet scaffold

--- src/domain/editorialPacketTypes.js
+++
@@ -1,8 +0,0 @@
-export const EDITORIAL_PACKET_EVIDENCE_KEYS = Object.freeze([
-  'hasTeamComparison',
-  'hasPlayerStats',
-  'hasScoringSummary',
-  'hasRtg',
-  'hasOfficialMedia',
-  'hasMilestones',
-]);

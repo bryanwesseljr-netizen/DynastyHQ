a3a829a6721ac290071d4f9a0c3ebcebdeb4c647
bwesseljr <bryanwesseljr@gmail.com>
2026-09-14 22:53:57 -0400

    Add editorial packet evidence keys

---
+++ src/domain/editorialPacketTypes.js
@@ -0,0 +1,8 @@
+export const EDITORIAL_PACKET_EVIDENCE_KEYS = Object.freeze([
+  'hasTeamComparison',
+  'hasPlayerStats',
+  'hasScoringSummary',
+  'hasRtg',
+  'hasOfficialMedia',
+  'hasMilestones',
+]);

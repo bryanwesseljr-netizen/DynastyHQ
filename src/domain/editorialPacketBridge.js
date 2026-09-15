af72dfdf4cffbcc7927188ca12b1a7273ecbcf03
bwesseljr <bryanwesseljr@gmail.com>
2026-09-14 22:58:51 -0400

    Remove temporary editorial packet scaffold

--- src/domain/editorialPacketBridge.js
+++
@@ -1,7 +0,0 @@
-import { buildPublishedWeekEditorialPacket, editorialPacketFactRows } from './publishedWeekEditorialPacket.js';
-
-export const withPublishedWeekEditorialPacket = (state = {}, publicationId = '') => {
-  const packet = buildPublishedWeekEditorialPacket(state, publicationId);
-  const packetFacts = editorialPacketFactRows(packet);
-  return { packet, packetFacts };
-};

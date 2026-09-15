c003d3ca183a024804d4c3da8e4f1797ea285ea9
bwesseljr <bryanwesseljr@gmail.com>
2026-09-14 22:58:45 -0400

    Remove temporary editorial packet scaffold

--- src/domain/editorialPacketActivation.test.js
+++
@@ -1,5 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { EDITORIAL_PACKET_ACTIVE } from './editorialPacketNoop.js';
-
-test('editorial packet feature branch is active', () => assert.equal(EDITORIAL_PACKET_ACTIVE, true));

e8b78fcda0cf60a58d2e731f413fbecac7088378
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 11:02:14 -0400

    Split OwnerWeeklyEnhancements chunk

---
+++ src/components/OwnerWeeklyEnhancements.jsx
@@ -0,0 +1,29 @@
+import WeekSetupPortal from './WeekSetupPortal.jsx';
+import GameweekFlowPortal from './GameweekFlowPortal.jsx';
+import QuickImportPortal from './QuickImportPortal.jsx';
+import WeeklyAgendaV2Portal from './WeeklyAgendaV2Portal.jsx';
+import WeeklyDataIntakePortal from './WeeklyDataIntakePortal.jsx';
+import DryRunModePortal from './DryRunModePortal.jsx';
+import AiScanRoutingPortal from './AiScanRoutingPortal.jsx';
+import CoverageDataIntakePortal from './CoverageDataIntakePortal.jsx';
+import RtgStatusIntakePortal from './RtgStatusIntakePortal.jsx';
+import WeeklyAppearanceAndLocationPortal from './WeeklyAppearanceAndLocationPortal.jsx';
+import CollegeCareerAgendaCardPortal from './CollegeCareerAgendaCardPortal.jsx';
+
+const OwnerWeeklyEnhancements = () => (
+  <>
+    <WeekSetupPortal />
+    <GameweekFlowPortal />
+    <QuickImportPortal />
+    <WeeklyAgendaV2Portal />
+    <WeeklyDataIntakePortal />
+    <DryRunModePortal />
+    <AiScanRoutingPortal />
+    <CoverageDataIntakePortal />
+    <RtgStatusIntakePortal />
+    <WeeklyAppearanceAndLocationPortal />
+    <CollegeCareerAgendaCardPortal />
+  </>
+);
+
+export default OwnerWeeklyEnhancements;

023834aa9b5bc0d2e2664d8fe835a0441e91cf33
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 11:02:25 -0400

    Split OwnerNewsroomEnhancements chunk

---
+++ src/components/OwnerNewsroomEnhancements.jsx
@@ -0,0 +1,29 @@
+import EditorialPhotoDirectorPortal from './EditorialPhotoDirectorPortal.jsx';
+import EditorialLanguageRealismPortal from './EditorialLanguageRealismPortal.jsx';
+import NewsroomGameLocationPortal from './NewsroomGameLocationPortal.jsx';
+import NewsroomUniformContextPortal from './NewsroomUniformContextPortal.jsx';
+import NewsroomArticleExperiencePortal from './NewsroomArticleExperiencePortal.jsx';
+import NewsroomArticleRewritePortal from './NewsroomArticleRewritePortal.jsx';
+import NewsroomArticleToolsPortal from './NewsroomArticleToolsPortal.jsx';
+import NewsroomArticleSharePortal from './NewsroomArticleSharePortal.jsx';
+import NewsroomExactStoryRoutingPortal from './NewsroomExactStoryRoutingPortal.jsx';
+import NewsroomLibraryScrollGuardPortal from './NewsroomLibraryScrollGuardPortal.jsx';
+import NewsroomTeamHubPortal from './NewsroomTeamHubPortal.jsx';
+
+const OwnerNewsroomEnhancements = () => (
+  <>
+    <EditorialPhotoDirectorPortal />
+    <EditorialLanguageRealismPortal />
+    <NewsroomGameLocationPortal />
+    <NewsroomUniformContextPortal />
+    <NewsroomExactStoryRoutingPortal />
+    <NewsroomLibraryScrollGuardPortal />
+    <NewsroomTeamHubPortal />
+    <NewsroomArticleExperiencePortal />
+    <NewsroomArticleRewritePortal />
+    <NewsroomArticleToolsPortal />
+    <NewsroomArticleSharePortal />
+  </>
+);
+
+export default OwnerNewsroomEnhancements;

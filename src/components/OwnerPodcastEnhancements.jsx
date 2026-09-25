c5a5a3cdcccd6c4e464f1e9684185c54824cb5ca
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 11:02:29 -0400

    Split OwnerPodcastEnhancements chunk

---
+++ src/components/OwnerPodcastEnhancements.jsx
@@ -0,0 +1,19 @@
+import PodcastArtworkHydrationPortal from './PodcastArtworkHydrationPortal.jsx';
+import PodcastLocalShowPortal from './PodcastLocalShowPortal.jsx';
+import PodcastHumanizedAudioPortal from './PodcastHumanizedAudioPortal.jsx';
+import PodcastMasterAudioPortalV2 from './PodcastMasterAudioPortalV2.jsx';
+import PodcastFinishedAudioIntegrationPortal from './PodcastFinishedAudioIntegrationPortal.jsx';
+import PodcastSeekControlsPortal from './PodcastSeekControlsPortal.jsx';
+
+const OwnerPodcastEnhancements = () => (
+  <>
+    <PodcastArtworkHydrationPortal />
+    <PodcastLocalShowPortal />
+    <PodcastHumanizedAudioPortal />
+    <PodcastMasterAudioPortalV2 />
+    <PodcastFinishedAudioIntegrationPortal />
+    <PodcastSeekControlsPortal />
+  </>
+);
+
+export default OwnerPodcastEnhancements;

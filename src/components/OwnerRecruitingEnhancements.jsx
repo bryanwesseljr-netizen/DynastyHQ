afc8de6f3d46a85cb29e1c0f0eb5de6190398f7d
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 12:13:06 -0400

    Remove unstable split OwnerRecruitingEnhancements.jsx

--- src/components/OwnerRecruitingEnhancements.jsx
+++
@@ -1,5 +0,0 @@
-import CoachRecruitingWorkspaceV2Portal from './CoachRecruitingWorkspaceV2Portal.jsx';
-
-const OwnerRecruitingEnhancements = () => <CoachRecruitingWorkspaceV2Portal />;
-
-export default OwnerRecruitingEnhancements;

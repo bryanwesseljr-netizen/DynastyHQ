fa50c56c362614160962ded240a4905cce97c847
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 11:57:34 -0400

    Update podcast integration mount test for feature split

--- src/domain/podcastFinishedAudioIntegration.test.js
+++ src/domain/podcastFinishedAudioIntegration.test.js
@@ -5,7 +5,7 @@ import test from 'node:test';
 const masterUrl = new URL('../components/PodcastMasterAudioPortalV2.jsx', import.meta.url);
 const studioUrl = new URL('../components/PodcastStudio.jsx', import.meta.url);
 const integrationUrl = new URL('../components/PodcastFinishedAudioIntegrationPortal.jsx', import.meta.url);
-const ownerUrl = new URL('../components/OwnerEnhancements.jsx', import.meta.url);
+const ownerUrl = new URL('../components/OwnerPodcastEnhancements.jsx', import.meta.url);
 
 test('finished podcast workflow accepts NotebookLM M4A and archives it as continuous ready audio', async () => {
   const master = await readFile(masterUrl, 'utf8');

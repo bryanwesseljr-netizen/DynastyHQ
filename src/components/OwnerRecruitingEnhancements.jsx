428cef02742ada308be53e11e524e499d36f2f66
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 11:02:23 -0400

    Split OwnerRecruitingEnhancements chunk

---
+++ src/components/OwnerRecruitingEnhancements.jsx
@@ -0,0 +1,5 @@
+import CoachRecruitingWorkspaceV2Portal from './CoachRecruitingWorkspaceV2Portal.jsx';
+
+const OwnerRecruitingEnhancements = () => <CoachRecruitingWorkspaceV2Portal />;
+
+export default OwnerRecruitingEnhancements;

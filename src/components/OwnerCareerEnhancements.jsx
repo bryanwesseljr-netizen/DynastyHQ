d3b002ac4dc0eb8e7b79fb897c516a595c1edcb7
bwesseljr <bryanwesseljr@gmail.com>
2026-09-25 11:02:21 -0400

    Split OwnerCareerEnhancements chunk

---
+++ src/components/OwnerCareerEnhancements.jsx
@@ -0,0 +1,17 @@
+import CareerOverviewViewportPortal from './CareerOverviewViewportPortal.jsx';
+import CareerStandbyPortal from './CareerStandbyPortal.jsx';
+import StandbyStatusUpdatePortal from './StandbyStatusUpdatePortal.jsx';
+import CareerEditorialEventPortal from './CareerEditorialEventPortal.jsx';
+import CareerMuseumPortal from './CareerMuseumPortal.jsx';
+
+const OwnerCareerEnhancements = () => (
+  <>
+    <CareerOverviewViewportPortal />
+    <CareerStandbyPortal />
+    <StandbyStatusUpdatePortal />
+    <CareerEditorialEventPortal />
+    <CareerMuseumPortal />
+  </>
+);
+
+export default OwnerCareerEnhancements;
